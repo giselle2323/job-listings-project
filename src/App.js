a55d9f782c351f412cf180e3a4ab1650f76e845b
giselle2323 <yakubuaminat94@gmail.com>
2020-07-17 21:33:28 +0100

    mobile design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
+import {Data} from './data'
 import NavBar from './components/NavBar';
+import Card from './components/Card';
 
 function App() {
+
+  const [data] = useState(Data);
   return (
     <div className="App">
       <NavBar />
+      <div className='job-listings-container'>
+        {data.map(job => (
+          job.featured ? <Card key={job.id} data={job} className={'featured-job'}/>  : <Card key={job.id} data={job} />
+          
+        ))}
+      </div>
     </div>
   );
 }
