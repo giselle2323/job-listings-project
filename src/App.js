e8bc407c75617089891c99971b49c577b3160850
giselle2323 <yakubuaminat94@gmail.com>
2020-08-02 23:31:00 +0100

    filter container logic

--- src/App.js
+++ src/App.js
@@ -10,36 +10,51 @@ const App = ()  => {
   const [query, setQuery] = useState([]);
 
   const handleClick = (e) => {
-    e.persist()
-    setQuery(queryText => [e.target.value, ...queryText])
-    filterFunction()
-    console.log(data);
-    return (
-      <div className='modal'>
-        <div className='filter-container'>
-          {query.map(queryText => <button className={`card-button-label ${queryText}`} key={queryText}>{queryText}</button>)}
-        </div>
-      </div>
-    )
-  }
+    const buttonValue = e.target.value;
+    const val = query.filter(text => text === buttonValue)
+    let filterVal = query;
+    if (val.length === 0) {
+      filterVal = query.concat(buttonValue)
+      setQuery(filterVal)
+    }
 
-  const filterFunction = () => {
-    const filtered = data.filter(job => query.length ?
-      query.includes(job.role) ||
-      query.includes(job.level) ||
-      job.tools.some(tool => query.includes(tool)) ||
-      job.languages.some(language => query.includes(language))
+    filterFunction(filterVal)
+  }
+  const removeButton = (e) => {
+    let buttonValue = e.target.value;
+    let filteredItems = query.filter(text => text !== buttonValue)
+    console.log(filteredItems, 'remove')
+    setQuery(filteredItems);
+
+  }
+  const filterFunction = (filterVal) => {
+    setData(data);
+    const filtered = data.filter(job => filterVal.length ?
+      filterVal.includes(job.role) ||
+      filterVal.includes(job.level) ||
+      job.tools.some(tool => filterVal.includes(tool)) ||
+      job.languages.some(language => filterVal.includes(language))
       : data
     )
     setData(filtered);
+    console.log(filtered);
   }
+  console.log(query, 'outside')
   return (
     <div className="App">
       <NavBar />
+      {/* Correction */}
+      {
+        query.length > 0 && (
+            <div className='filter-container'>
+              {query.map((queryText, key) => <button className={`card-button-label ${queryText}`} key={key} value={queryText}>{queryText} <span className='close__button' onClick={removeButton}>X</span> </button>)}
+            </div>
+        )
+      }
       <div className='job-listings-container'>
         {data.map(job => (
           job.featured ? <Card key={job.id} data={job} className={'featured-job'} onClickFunction={handleClick} /> : <Card key={job.id} data={job} onClickFunction={handleClick} />
-          
+
         ))}
       </div>
     </div>
